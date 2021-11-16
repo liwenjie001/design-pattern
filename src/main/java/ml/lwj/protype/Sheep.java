9467bf469261145274bc21d3978ca1a00d9cb780
wenjie.li <wenjie.li@ninebot.com>
2021-11-16 23:23:21 +0800

    原型模式

---
+++ src/main/java/ml/lwj/protype/Sheep.java
@@ -0,0 +1,60 @@
+package ml.lwj.protype;
+
+public class Sheep implements Cloneable{
+    private String name;
+    private int age;
+    private String color;
+
+    public Sheep(String name, int age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Sheep{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", color='" + color + '\'' +
+                '}';
+    }
+    /*
+      克隆使用默认的方法
+      这种是潜拷贝
+     */
+    @Override
+    protected Sheep clone() {
+
+        try {
+            return (Sheep) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
