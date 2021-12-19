b8aa3ac0177c95419f15408a5da061076ebfeab5
liwenjie001 <1554616416@qq.com>
2021-12-19 23:41:51 +0800

    '类适配器'

---
+++ src/main/java/ml/lwj/adapter/classadapter/IVoltage5V.java
@@ -0,0 +1,8 @@
+package ml.lwj.adapter.classadapter;
+
+/**
+ * 适配接口
+ */
+public interface IVoltage5V {
+    public int output5V();
+}
---
+++ src/main/java/ml/lwj/adapter/classadapter/Voltage220V.java
@@ -0,0 +1,12 @@
+package ml.lwj.adapter.classadapter;
+
+/**
+ * 被适配的类
+ */
+public class Voltage220V {
+    public int output220V(){
+        int src = 220;
+        System.out.println("电压="+src + "伏");
+        return src;
+    }
+}
